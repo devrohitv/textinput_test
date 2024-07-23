7020208d5e0cd62d6f3b18f285a9025918b5e3aa
devrohitv <142287858+devrohitv@users.noreply.github.com>
2024-07-23 17:23:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from normal_input import NormalInput
+from text_input import MyTextInput
+
+class Layout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(NormalInput())
+        self.add_widget(MyTextInput())
+        self.add_widget(Widget(size_hint=(1, .5)))
+
+class MainApp(App):
+    def build(self):
+        return Layout()
+
+MainApp().run()
---
+++ text_input.py
@@ -0,0 +1,28 @@
+from kivy.utils import platform
+from kivy.uix.textinput import TextInput
+if platform == "android":
+    from jnius import autoclass
+class MyTextInput(TextInput):
+    # keyboard_suggestions's on_ doesn't work..
+    __events__ = ('on_suggestions', )
+
+    def on_kv_post(self, instance):
+        if platform == "android":
+            self.dispatch('on_suggestions')
+
+    def on_suggestions(self, instance=None, value=None):
+        Build = autoclass("android.os.Build$VERSION")
+        if Build.SDK_INT >= 33 and self.keyboard_suggestions:
+            PythonActivity = autoclass('org.kivy.android.PythonActivity')
+            Context = autoclass('android.content.Context')
+            imm = PythonActivity.mActivity.getSystemService(Context.INPUT_METHOD_SERVICE)
+            currentSubtype = imm.getCurrentInputMethodSubtype()
+
+            if currentSubtype is not None:
+                extra = currentSubtype.getExtraValue()
+                if currentSubtype.getMode() == "keyboard" and any(["TrySuppressingImeSwitcher" in extra,
+                                                                   "AsciiCapable" in extra]):
+                    self.keyboard_suggestions = False
+                    print("Kivys keyboard_suggestions for Gboard is now deactivated because it's already activated")
+                    return
+        print("No changes applied for the keyboard_suggestions.")
