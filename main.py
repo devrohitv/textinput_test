50860566a3763bb31425f3979cc72998079b6f77
devrohitv <142287858+devrohitv@users.noreply.github.com>
2024-07-24 10:35:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,21 +9,6 @@ class Layout(GridLayout):
         super().__init__(**kwargs)
         self.cols = 1
         self.add_widget(NormalInput())
-        self.add_widget(KuTextInput())
-        self.add_widget(Widget(size_hint=(1, .5)))
-
-class MainApp(App):
-    def build(self):
-        return Layout()
-
-MainApp().run()
-
-
-class Layout(GridLayout):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.cols = 1
-        self.add_widget(NormalInput())
         self.add_widget(MyTextInput())
         self.add_widget(Widget(size_hint=(1, .5)))
 
