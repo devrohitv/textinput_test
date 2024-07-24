ace7403eb7eaf23932f41d413285e02d4c9deeda
devrohitv <142287858+devrohitv@users.noreply.github.com>
2024-07-24 09:36:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,26 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
 from normal_input import NormalInput
-from text_input import MyTextInput
+from text_input import MyTextInputfrom kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from normal_input import NormalInput
+from text_input_kuze import KuTextInput
+
+class Layout(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(NormalInput())
+        self.add_widget(KuTextInput())
+        self.add_widget(Widget(size_hint=(1, .5)))
+
+class MainApp(App):
+    def build(self):
+        return Layout()
+
+MainApp().run()
+
 
 class Layout(GridLayout):
     def __init__(self, **kwargs):
